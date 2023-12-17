12a7c8615d2609a4a4fd58ca0d611ebdf1958264
tinotendnyashanu <tinotendanyash@gmail.com>
2023-12-17 11:26:55 +0100

    Refactor webhook route and update mobile navigation

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -5,18 +5,17 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet"
-import Image from "next/image"
-import { Separator } from "../ui/separator"
-import NavItems from "./NavItems"
-
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { Separator } from "../ui/separator";
+import NavItems from "./NavItems";
 
 const MobileNav = () => {
   return (
     <nav className="md:hidden">
       <Sheet>
         <SheetTrigger className="align-middle">
-          <Image 
+          <Image
             src="/assets/icons/menu.svg"
             alt="menu"
             width={24}
@@ -25,8 +24,8 @@ const MobileNav = () => {
           />
         </SheetTrigger>
         <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
-          <Image 
-            src="/assets/images/logo.svg"
+          <Image
+            src="/assets/images/logo.png"
             alt="logo"
             width={128}
             height={38}
@@ -36,7 +35,7 @@ const MobileNav = () => {
         </SheetContent>
       </Sheet>
     </nav>
-  )
-}
+  );
+};
 
-export default MobileNav
+export default MobileNav;
