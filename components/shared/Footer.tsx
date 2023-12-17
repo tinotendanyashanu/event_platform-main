65758113aa9d4b5e8e558db4326e650d371bdd2d
tinotendnyashanu <tinotendanyash@gmail.com>
2023-12-17 09:47:44 +0100

    Update favicon, hero image, and database connection
    Update footer logo and copyright text
    
    Update environment variables
    
    Update header logo and login button

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,23 +1,23 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
-        <Link href='/'>
-          <Image 
-            src="/assets/images/logo.svg"
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.png"
             alt="logo"
-            width={128}
+            width={50}
             height={38}
           />
         </Link>
 
-        <p>2023 Evently. All Rights reserved.</p>
+        <p>2023 Sanganai. All Rights reserved.</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
